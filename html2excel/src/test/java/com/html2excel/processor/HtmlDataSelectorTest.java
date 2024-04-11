0ef0fb6e4fe81ef61380dc0caf81c23b38339b5d
albus_percival <sergeytarasevich2012@gmail.com>
2024-04-11 05:33:55 +0200

    rename test package

---
+++ html2excel/src/test/java/com/html2excel/processor/HtmlDataSelectorTest.java
@@ -0,0 +1,12 @@
+package com.html2excel.processor;
+
+import org.junit.jupiter.api.Test;
+
+public class HtmlDataSelectorTest {
+
+	
+	@Test
+	void htmlParseTest() {
+
+	}
+}
--- html2excel/src/test/java/html2excelcom/html2excel/processor/HtmlDataSelectorTest.java
+++
@@ -1,5 +0,0 @@
-package html2excelcom.html2excel.processor;
-
-public class HtmlDataSelectorTest {
-
-}
